7007bec931cabdd4621d2ef7b4b0ba8cc6f2b013
sundaram08 <sundaramdwivedi2003@gmail.com>
2024-06-12 20:26:31 +0530

    sub-routes & Sidebar

--- onwe-next/src/components/SideBar/SideBar.tsx
+++ onwe-next/src/components/SideBar/SideBar.tsx
@@ -1,47 +1,63 @@
-// Navigation.tsx
-import Link from "next/link";
-import React from "react";
+import Link from 'next/link';
+import React from 'react';
+import { Home, Users, Compass, Search, Newspaper, Calendar, Bell, Plus, User, Settings } from 'lucide-react';
 
 const SideBar = () => {
   return (
-    <div className="left-0 top-0 h-screen w-40 bg-gray-200 flex flex-col justify-between items-center py-4">
-      <div className="flex flex-col items-center">
-        <Link href="/home" className="mb-6 text-gray-700 hover:text-gray-900">
-          <h1 className="font-bold">Home</h1>
-        </Link>
-        <Link
-          href="/profile"
-          className="mb-6 text-gray-500 hover:text-gray-900"
-        >
-          <h1 className="font-bold">Profile</h1>
-        </Link>
-        <Link
-          href="/explore"
-          className="mb-6 text-gray-500 hover:text-gray-900"
-        >
-          <h1 className="font-bold">Explore</h1>
-        </Link>
-        <Link
-          href="/notifications"
-          className="mb-6 text-gray-500 hover:text-gray-900"
-        >
-          <h1 className="font-bold">Notifications</h1>
-        </Link>
-        <Link
-          href="/messages"
-          className="mb-6 text-gray-500 hover:text-gray-900"
-        >
-          <h1 className="font-bold">Messages</h1>
-        </Link>
-        <Link href="/saved" className="mb-6 text-gray-500 hover:text-gray-900">
-          <h1 className="font-bold">Saved</h1>
+    <div className="h-screen w-40  flex flex-col items-center p-4 justify-between bg-[#f1f3f5]">
+      <div className="flex items-start justify-center">
+        <Link href="/" className="text-gray-700 hover:text-gray-900">
+          <h1 className="font-bold text-2xl">ONWE</h1>
         </Link>
       </div>
-      <div className="mb-6">
-        <Link href="#">LogOut</Link>
+      <hr className="border-t border-black my-4 w-full" />
+      <div className="flex flex-col items-start w-full mt-10 space-y-5">
+        <Link href="/home" className=" flex items-center text-black hover:text-gray-500 w-full">
+          <Home size={20} strokeWidth={1.4} />
+          <h1 className="ml-2 text-sm">Home</h1>
+        </Link>
+        <Link href="/profile" className=" flex items-center text-black hover:text-gray-500 w-full">
+          <Users strokeWidth={1.4} size={20} />
+          <h1 className="ml-2 text-sm">Clubs</h1>
+        </Link>
+        <Link href="/explore" className=" flex items-center text-black hover:text-gray-500 w-full">
+          <Compass size={20} strokeWidth={1.4} />
+          <h1 className="ml-2 text-sm">Explore</h1>
+        </Link>
+        <Link href="/notifications" className=" flex items-center text-black hover:text-gray-500 w-full">
+          <Search size={20} strokeWidth={1.4} />
+          <h1 className="ml-2 text-sm">Search</h1>
+        </Link>
+        <Link href="/messages" className=" flex items-center text-black hover:text-gray-500 w-full">
+          <Newspaper size={20} strokeWidth={1.4} />
+          <h1 className="ml-2 text-sm">Magazines</h1>
+        </Link>
+        <Link href="/saved" className=" flex items-center text-black hover:text-gray-500 w-full">
+          <Calendar size={20} strokeWidth={1.4} />
+          <h1 className="ml-2 text-sm">Event</h1>
+        </Link>
+        <Link href="/saved" className=" flex items-center text-black hover:text-gray-500 w-full">
+          <Bell size={20} strokeWidth={1.4} />
+          <h1 className="ml-2 text-sm">Notifications</h1>
+        </Link>
+        <Link href="/saved" className=" flex items-center text-black hover:text-gray-500 w-full">
+          <Plus size={20} strokeWidth={1.4} />
+          <h1 className="ml-2 text-sm">Create</h1>
+        </Link>
+      </div>
+      <div className="flex flex-col items-start w-full  mt-40">
+        <Link href="/profile" className="text-black hover:text-gray-900 mb-2 flex items-center w-full">
+          <User strokeWidth={1.4} />
+          <h1 className="ml-2 text-sm">Profile</h1>
+        </Link>
+        <Link href="/settings" className="text-black hover:text-gray-900 flex items-center w-full">
+          <Settings strokeWidth={1.4} />
+          <h1 className="ml-2 text-sm">Settings</h1>
+        </Link>
       </div>
     </div>
   );
 };
 
 export default SideBar;
+

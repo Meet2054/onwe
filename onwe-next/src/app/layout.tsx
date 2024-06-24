e4bc53bf17f236e6478051b8bce0da485c8bc32b
rituisboy <riturajthakur100@gmail.com>
2024-06-24 13:51:35 +0530

    layout changes

--- onwe-next/src/app/layout.tsx
+++ onwe-next/src/app/layout.tsx
@@ -17,21 +17,31 @@ interface RootLayoutProps {
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   const pathname = usePathname();
   const showSideBar = pathname.startsWith("/home");
-  const showMinSideBar = pathname !== "/" && pathname !== "/sign-up" && pathname !== "/sign-in" && !pathname.startsWith("/home");
-    return (
-      <ClerkProvider>
+  const showMinSideBar =
+    pathname !== "/" &&
+    pathname !== "/sign-up" &&
+    pathname !== "/sign-in" &&
+    !pathname.startsWith("/home");
+  return (
+    <ClerkProvider>
       <Provider store={store}>
-      <html lang="en" className="bg-[#f1f3f5]">
-        <body className={`${inter.className} h-screen overflow-hidden`}>
-         <div className="flex h-full">
-         {showSideBar && <div className="w-3/12"><SideBar /></div>}
-         {showMinSideBar && <div className="w-1/6"><MinSideBar /></div>}
-           <div className="flex-1 h-full overflow-y-auto">
-             {children}
-           </div>
-         </div>
-        </body>
-      </html>
+        <html lang="en" className="bg-white">
+          <body className={`${inter.className} h-screen overflow-hidden`}>
+            <div className="flex ">
+              {showSideBar && (
+                <div className="w-3/12">
+                  <SideBar />
+                </div>
+              )}
+              {showMinSideBar && (
+                <div className="w-1/6">
+                  <MinSideBar />
+                </div>
+              )}
+              <div className="flex-1 h-full overflow-y-auto">{children}</div>
+            </div>
+          </body>
+        </html>
       </Provider>
     </ClerkProvider>
   );

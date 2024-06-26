8070dade371a22e620bae63a271747aff7b841fb
rituisboy <riturajthakur100@gmail.com>
2024-06-26 19:03:00 +0530

    overflow-fix

--- onwe-next/src/app/events/page.tsx
+++ onwe-next/src/app/events/page.tsx
@@ -23,52 +23,46 @@ const Page: React.FC = () => {
   return (
     <div className="flex">
       <div className="bg-white flex flex-col w-3/5">
-        <div className="top-0 sticky bg-white p-4 w-max ml-12 ">
-          <ul className="flex space-x-8">
+        <div className="top-0 sticky bg-white p-4  ml-12 ">
+          <div className="flex space-x-6 ml-4">
             <button
               onClick={() => handleFilter("upcoming")}
-              className="border rounded-full text-sm p-2 active:bg-black"
+              className="border rounded-full text-sm p-1 active:bg-black"
             >
               Upcoming Events
             </button>
             <button
               onClick={() => handleFilter("past")}
-              className="border rounded-full text-sm p-2 active:bg-black"
+              className="border rounded-full text-sm p-1 active:bg-black"
             >
               Past Events
             </button>
             <button
               onClick={() => handleFilter("remainder")}
-              className="border rounded-full text-sm p-2 active:bg-black"
+              className="border rounded-full text-sm p-1 active:bg-black"
             >
               My Remainders
             </button>
-            <button
-              onClick={() => handleFilter("saved")}
-              className="border rounded-full text-sm p-2 active:bg-black"
-            >
-              Saved
-            </button>
+
             <button
               onClick={() => setListView(!listview)}
-              className="border rounded-full text-sm p-2 active:bg-black"
+              className="border rounded-full text-sm p-1 ml-20  active:bg-black"
             >
               List View
             </button>
-          </ul>
+          </div>
         </div>
-        <div className="p-4 w-full overflow-y-auto h-screen ml-12" style={{ maxHeight: 'calc(100vh)' }}>
-        <div
-          className="p-4 mb-2 w-full relative overflow-y-auto h-screen"
-          style={{ maxHeight: "calc(100vh)" }}
-        >
-          {/* Adjust maxHeight value as needed */}
-          {events.map((event: Event) => (
-            <EventCard2 key={event.id} {...event} />
-          ))}
+        <div className="p-4 w-full h-screen ml-12 ">
+          <div className="p-4 mb-2 w-full relative overflow-y-auto h-full ">
+            {/* Adjust maxHeight value as needed */}
+            {events.map((event: Event) => (
+              <EventCard2 key={event.id} {...event} />
+            ))}
+            <div className="size-20"></div>
+          </div>
         </div>
       </div>
-      <div className="w-2/5 h-[100dvh] overflow-y-auto">
+      <div className="w-2/5">
         <RenderCalendar />
       </div>
     </div>

13ca7fa63c0f1cb65e989219b29f9480e0b16143
sundaram08 <sundaramdwivedi2003@gmail.com>
2024-06-26 10:46:18 +0530

    EventCard

--- onwe-next/src/components/clubs/ClubCard.tsx
+++ onwe-next/src/components/clubs/ClubCard.tsx
@@ -13,11 +13,11 @@ interface ClubCardProps {
 
 const ClubCard: React.FC<ClubCardProps> = ({ club }) => {
   return (
-    <div className="flex items-center mt-2 ml-4 p-2">
+    <div className="flex items-center mt-2 ml-6 p-2">
       <div className="rounded-full bg-gray-300 h-10 w-10 mr-3" style={{ backgroundImage: `url(${club.image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
       <div>
         <Link href={`/clubs/${club.name}`} className="text-gray-700 hover:text-black">
-        <h2 className="text-sm">{club.name}</h2>
+        <h2 className="text-sm font-bold">{club.name}</h2>
             {/* <img src="" alt="" /> */}
         </Link>
       </div>

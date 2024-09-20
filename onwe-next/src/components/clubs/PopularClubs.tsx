bdb16860f3d7f9cc904ae616383e94c4f221eba2
rituisboy <riturajthakur100@gmail.com>
2024-09-20 10:44:23 +0530

    refactor: Update PopularClubs and PopularClubsCard components
    - Add new interface AllClub to handle different types of clubs
    - Update PopularClubs component to dynamically set the title based on the type prop
    - Update PopularClubsCard component to handle different types of clubs and display the appropriate content

--- onwe-next/src/components/clubs/PopularClubs.tsx
+++ onwe-next/src/components/clubs/PopularClubs.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PopularClubsCard, { PopularClubsCardSkeleton } from "./PopularClubsCard";
 import axios from "axios";
 import useSWR from "swr";
@@ -13,26 +13,42 @@ export interface Club {
   slogan: string;
   updatedAt: string; // ISO 8601 date string
 }
-
-const fetcher = async (url: string) => {
-  const response = await axios.get(process.env.NEXT_PUBLIC_API_URL + url, {
-    headers: { Authorization: "Bearer " + localStorage.getItem("token") },
-  });
-  return response.data;
-};
+export interface AllClub {
+  clubId: number;
+  clubName: string;
+  coverImage: string[];
+  slogan: string;
+}
 
 interface PopularClubsProps {
   type: "/popular" | "/trending" | "/clubs/all";
 }
 
+const fetcher = async (url: string) => {
+  const response = await axios.get(process.env.NEXT_PUBLIC_API_URL + url, {
+    headers: { Authorization: "Bearer " + localStorage.getItem("onwetoken") },
+  });
+  return response.data;
+};
+
 const PopularClubs = ({ type }: PopularClubsProps) => {
   const { data: clubs, error, isLoading } = useSWR<Club[]>(`${type}`, fetcher);
+  const [title, settitle] = useState("");
+  useEffect(() => {
+    if (type == "/popular") {
+      settitle("Popular");
+    } else if (type == "/trending") {
+      settitle("Trending");
+    } else if (type == "/clubs/all") {
+      settitle("All Clubs");
+    }
+  }, []);
 
   if (!clubs || isLoading) {
     return (
       <div className="p-2  mt-3 ">
         <div className="space-x-2">
-          <span className="text-lg font-bold">Popular</span>
+          <span className="text-lg font-bold">{title}</span>
           <span className="px-4 py-[2px] text-base  font-semibold rounded-full border-2 border-black shadow-[3px_3px_0_0_#000]">
             CLUB
           </span>
@@ -46,20 +62,18 @@ const PopularClubs = ({ type }: PopularClubsProps) => {
       </div>
     );
   }
-  if (clubs) {
-    console.log(clubs);
-  }
+
   return (
     <div className="p-2  mt-3 ">
       <div className="space-x-2">
-        <span className="text-lg font-bold">Popular</span>
+        <span className="text-lg font-bold">{title}</span>
         <span className="px-4 py-[2px] text-base  font-semibold rounded-full border-2 border-black shadow-[3px_3px_0_0_#000]">
           CLUB
         </span>
       </div>
       <div className="w-full  mt-3 grid grid-cols-4 gap-3">
         {clubs.map((club) => (
-          <PopularClubsCard key={club.id} club={club} />
+          <PopularClubsCard key={club.id} club={club} type={type} />
         ))}
       </div>
     </div>
--- onwe-next/src/components/clubs/PopularClubsCard.tsx
+++ onwe-next/src/components/clubs/PopularClubsCard.tsx
@@ -1,32 +1,56 @@
 import { base64Prefix } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
-import { Club } from "./PopularClubs";
+import { AllClub, Club } from "./PopularClubs";
 import { Skeleton } from "../ui/skeleton";
 import Link from "next/link";
 
-const PopularClubsCard = ({ club }: { club: Club }) => {
-  return (
-    <Link
-      href={`/clubs/${club.name}`}
-      className="hover:scale-105 w-full flex gap-x-2
-      hover:shadow-[0_1px_0_0_#000]  animate-in zoom-in-0 
-      items-center justify-between p-3  h-24 border rounded-lg border border-black transition-all ease-in-out duration-150 hover:shadow-md"
-    >
-      <div className="w-20 aspect-square  rounded-md relative">
-        <Image src={`${base64Prefix}${club.coverimage}`} fill alt="image" />
-      </div>
-      <div className="w-full h-full rounded-md">
-        <div className="text-xl font-bold">{club.name}</div>
-        <div
-          className="word-wrap
-            "
-        >
-          {club.slogan}
+const PopularClubsCard = ({ club, type }: { club: any; type: string }) => {
+  if (type == "/trending")
+    return (
+      <Link
+        href={`/clubs/${club.name}`}
+        className="hover:scale-105 border flex gap-x-1 h-14   rounded-md border-black transition-all ease-in-out duration-150 "
+      >
+        <div className="w-16 aspect-square  rounded-md relative">
+          <Image src={`${base64Prefix}${club.coverimage}`} fill alt="image" />
         </div>
-      </div>
-    </Link>
-  );
+        <div className="w-full h-full rounded-md">
+          <div className="text-xl font-bold">{club.name}</div>
+          <div
+            className="word-wrap
+            "
+          >
+            {club.slogan}
+          </div>
+        </div>
+      </Link>
+    );
+  if (type == "/clubs/all") {
+    return (
+      <Link
+        href={`/clubs/${club.name}`}
+        className="hover:scale-105 border flex gap-x-1 h-14 overflow-hidden  rounded-md border-black transition-all ease-in-out duration-150 "
+      >
+        <div className="w-20 aspect-square  rounded-md relative">
+          <Image
+            src={`${base64Prefix}${club.coverImage[0]}`}
+            fill
+            alt="image"
+          />
+        </div>
+        <div className="w-full h-full rounded-md">
+          <div className="text-xl font-bold">{club.clubName}</div>
+          <div
+            className="word-wrap
+            "
+          >
+            {club.slogan}
+          </div>
+        </div>
+      </Link>
+    );
+  }
 };
 export const PopularClubsCardSkeleton = () => {
   return (

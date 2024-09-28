009e0b97c2353c45efd082a5360df332de8eacbd
Guna Sai <saiguna2004@gmail.com>
2024-09-28 14:43:33 +0530

    Announcements 90%

--- onwe-next/src/components/clubs/Announcement.tsx
+++ onwe-next/src/components/clubs/Announcement.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import CreatePost from "./CreatePost";
 import { PostsProps } from "@/types/type";
@@ -7,32 +7,29 @@ import { useDispatch } from "react-redux";
 import { setPost } from "@/lib/features/posts/postSlice";
 import { useSignIn } from "@/hooks/useSignIn";
 import { NewPost } from "./NewPost";
+import PostAvatar from "../post_component/PostAvatar";
+import PostImage from "../post_component/PostImage";
 
 // const Announcement = ({ posts, club }: { posts: PostsProps[], club: string, isAdmin: boolean }) => {
 const Announcement = ({ club }: { club: string }) => {
-  const { getToken } = useSignIn();
+  const { getToken, getUsername } = useSignIn();
   const [createActive, setCreateActive] = useState(false);
   const [isAdmin, setisAdmin] = useState(false);
   const [posts, setPosts] = useState<PostsProps[]>([]);
+  const username = getUsername();
+  const postRefs = useRef<{ [key: number]: HTMLDivElement | null }>({});
 
-  // useEffect(() => {
-  //   const fetchPermission = async () => {
-  //     try {
-  //       const token = await getToken();
-  //       const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/myclubs/${club}`, {
-  //         headers: {
-  //           Authorization: `Bearer ${token}`,
-  //           "ngrok-skip-browser-warning": "69420",
-  //         },
-  //       });
-  //       setisAdmin(response.data);
-  //     } catch (err) {
-  //       console.error("Failed to fetch permission:", err);
-  //     }
-  //   };
+  useEffect(() => {
+    if (posts.length > 0) {
+      // Find the latest post by ID
+      const latestPostId = Math.max(...posts.map(post => post.id));
+      const latestPostRef = postRefs.current[latestPostId];
 
-  //   fetchPermission();
-  // }, [club, getToken]);
+      if (latestPostRef) {
+        latestPostRef.scrollIntoView();
+      }
+    }
+  }, [posts]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -49,7 +46,7 @@ const Announcement = ({ club }: { club: string }) => {
         );
         setPosts(response.data.posts);
         setisAdmin(response.data.isAdmin);
-        console.log(isAdmin,"raaaa");
+        console.log(posts)
         
       } catch (err: any) {
         console.log(err);
@@ -74,18 +71,46 @@ const Announcement = ({ club }: { club: string }) => {
   };
 
   return (
-    <div className="relative">
-      {posts.map((post) => (
-        <div
-          onClick={() => handleClick(post)}
-          key={post.id}
-          className="relative size-96"
-        >
-          {/* <h2>{post.id}</h2> */}
-          <DialogBox imageUrl={post.media[0]} post={post} />
-          {/* <p>{post.content}</p> */}
-        </div>
-      ))}
+    <div className="flex gap-20 flex-col bg-white grow min-h-full  pt-24 pl-10 pr-10 pb-24">
+    <div className="flex flex-col-reverse gap-3">
+      {posts.map((post) => {
+          let cnm = "";
+          if (post.username === username) {
+            cnm = "flex-row-reverse";
+          }
+
+          return (
+            <div
+              key={post.id}
+              ref={(el: HTMLDivElement | null) => { postRefs.current[post.id] = el; }} // Correctly assigning ref
+              className="w-full h-full"
+            >
+              <div className={`${cnm} flex`}>
+                <div
+                  onClick={() => handleClick(post)}
+                  className="grow max-w-60 sm:max-w-96 max-h-full rounded-xl p-2 bg-white border shadow"
+                >
+                  <div className="flex items-center gap-1 mt-4 mb-1">
+                    <PostAvatar imageUrl={post?.avatar} />
+                    <h1 className="font-semibold m-2 text-base">
+                      {post.username}
+                    </h1>
+                  </div>
+                  <div className=" pb-2 ">
+                    <PostImage
+                    fill="cover"
+                      images={post?.media}
+                      className="w-full relative h-64 overflow-hidden"
+                    />
+                  </div>
+                  <p className="m-2 ml-0 mt-[5px] p-2 w-[25vw] font-medium text-base rounded pl-2 whitespace-wrap break-all ">
+                    {post.description}
+                  </p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       {isAdmin && (
         <div className="fixed sm:bottom-0 bottom-16 bg-white pb-4">
         <div className="bg-white">
@@ -95,13 +120,14 @@ const Announcement = ({ club }: { club: string }) => {
       )}
       {/* {createActive && (
         <CreatePost
-          category="announcement"
-          clubName={club}
-          onClose={handleCloseClick}
+        category="announcement"
+        clubName={club}
+        onClose={handleCloseClick}
         />
       )} */}
       {/* need to keep check if isAdmin is true or not */}
       
+        </div>
     </div>
   );
 };

a1a313676b0d2e6c5b15abe1f4b455e9f221076b
rituisboy <riturajthakur100@gmail.com>
2024-10-05 12:27:54 +0530

    refactor: Update SingleComment component to include parentMutator prop

--- onwe-next/src/app/post/[id]/page.tsx
+++ onwe-next/src/app/post/[id]/page.tsx
@@ -1,50 +1,50 @@
 "use client";
 
-import DiaglogComment from "@/components/post_component/Dialog_component/DiaglogComment";
 import DialogBox from "@/components/post_component/Dialog_component/DialogBox";
-import DialogImage from "@/components/post_component/Dialog_component/DialogImage";
-import { useSignIn } from "@/hooks/useSignIn";
-import { cn } from "@/lib/utils";
 import { PostsProps } from "@/types/type";
-import axios from "axios";
-import { MessageSquare } from "lucide-react";
+import { setPost as setReduxPost } from "@/lib/features/posts/postSlice";
+
 import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import useSWR from "swr";
+import { useDispatch } from "react-redux";
+import Posts from "@/components/post_component/Posts";
+import { LoaderCircle } from "lucide-react";
 
 const Page = () => {
+  const postid = useParams().id;
+  const dispatch = useDispatch();
+  const [post, setPost] = useState<PostsProps | null>(null);
+  const { data, error, isLoading } = useSWR(`/posts/${postid}`, {
+    onSuccess: (data) => {
+      setPost(data);
+      dispatch(setReduxPost(data));
+    },
+  });
 
-    const {getToken} = useSignIn();
-    const postid =  useParams().id;
-    const [post, setPost] = useState<PostsProps|null>(null)
-    
-    useEffect(() => {
-        const getPost = async () => {
-            try {
-                const {data} = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/posts/${postid}`
-                , {
-                    headers: {
-                        Authorization: `Bearer ${ getToken()}`,
-                        "ngrok-skip-browser-warning": "69420",
-                    },
-                }
-                );
-                setPost(data as PostsProps)
-                console.log(post)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-         getPost()
-    }, [getToken,postid])
+  if (isLoading)
+    return (
+      <div className="w-full h-screen flex justify-center items-center">
+        <LoaderCircle size={30} className="animate-spin" />
+      </div>
+    );
 
-
-    return <div className="flex justify-center bg-white items-center text-black h-[80vh]">
-           {post ? <div className="w-[40vh] h-[40vh]"><DialogBox post={post} imageUrl={post.media[0]}  description={post.description}/></div>:
-            "No post found"
-           }
-           
-            
-    </div>;
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      {post ? (
+        <div className="w-1/2 shadow-lg rounded-md">
+          {/* <DialogBox
+            post={post}
+            imageUrl={post.media[0]}
+            description={post.description}
+          /> */}
+          <Posts post={post} />
+        </div>
+      ) : (
+        "No post found"
+      )}
+    </div>
+  );
 };
 
 export default Page;

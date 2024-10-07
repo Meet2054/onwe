daadc00378c9fb205674499e11c92438d9514f0a
rituisboy <riturajthakur100@gmail.com>
2024-10-07 16:43:17 +0530

    feat: Add toast notification for foreground push notifications

--- onwe-next/src/app/noti/page.tsx
+++ onwe-next/src/app/noti/page.tsx
@@ -1,11 +1,13 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getMessaging, onMessage } from "firebase/messaging";
 import firebaseApp from "@/lib/firebase";
 import useFcmToken from "@/hooks/FcmToken";
+import { toast } from "sonner";
 
 export default function Page() {
   const { fcmToken, notificationPermissionStatus } = useFcmToken();
+  const [noti, setMessage] = useState("");
   // Use the token as needed
   fcmToken && console.log("FCM token:", fcmToken);
 
@@ -15,6 +17,8 @@ export default function Page() {
       const messaging = getMessaging(firebaseApp);
       const unsubscribe = onMessage(messaging, (payload) => {
         console.log("Foreground push notification received:", payload);
+        setMessage(payload?.notification?.body as string);
+        toast(payload?.notification?.body as string);
         // Handle the received push notification while the app is in the foreground
         // You can display a notification or update the UI based on the payload
       });
@@ -24,5 +28,11 @@ export default function Page() {
     }
   }, []);
 
-  return <div> home </div>;
+  return (
+    <div className="h-96 break-all w-96">
+      {" "}
+      {fcmToken}
+      <span></span>
+    </div>
+  );
 }

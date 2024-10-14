e7aeeeb8c099cb00c4ecdf3f89e8e28c3446996c
rituisboy <riturajthakur100@gmail.com>
2024-10-14 14:07:41 +0530

    refactor: Add Firebase configuration for authentication and storage
    This commit adds the necessary Firebase configuration variables for authentication and storage in the `type.ts` file. The new variables include the Firebase API key, authentication domain, project ID, storage bucket, messaging sender ID, app ID, and measurement ID. These configurations enable the application to interact with Firebase services for user authentication and file storage.
    
    Resolves: #123

--- onwe-next/type.ts
+++ onwe-next/type.ts
@@ -7,9 +7,16 @@ declare namespace NodeJS {
     NEXT_PUBLIC_CLERK_FALLBACK_REDIRECT_URL: string;
     NEXT_PUBLIC_CLERK_FORCE_REDIRECT_URL: string;
     NEXT_PUBLIC_API_URL: string;
+    NEXT_PUBLIC_FIREBASE_API_KEY: string;
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: string;
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: string;
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
+    NEXT_PUBLIC_FIREBASE_APP_ID: string;
+    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: string;
   }
 }
-declare module '*.mp4' {
+declare module "*.mp4" {
   const src: string;
   export default src;
 }
